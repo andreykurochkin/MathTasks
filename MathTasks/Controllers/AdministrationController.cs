cbd64b58dbae5a82b4392f85b5374a22eb00c233
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-10-14 13:07:10 +0300

    added list of registered users

---
+++ MathTasks/Controllers/AdministrationController.cs
@@ -0,0 +1,22 @@
+﻿using MathTasks.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathTasks.Controllers {
+    public class AdministrationController : Controller {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdministrationController(UserManager<IdentityUser> userManager) {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() {
+            var users = _userManager.Users;
+            return View(users);
+        }
+    }
+}
