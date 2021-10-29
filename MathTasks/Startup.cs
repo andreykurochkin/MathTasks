1cb01d9723c74f7be7094f11238e3ed8d0d29a1f
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-10-29 10:26:56 +0300

    hard coded s3 credentials - update this feature later

--- MathTasks/Startup.cs
+++ MathTasks/Startup.cs
@@ -14,17 +14,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Amazon.S3;
 using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
 
-namespace MathTasks {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace MathTasks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -32,7 +38,8 @@ namespace MathTasks {
 
             //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             //    .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddIdentity<IdentityUser, IdentityRole>(options => { 
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
                 options.SignIn.RequireConfirmedAccount = true;
                 options.User.RequireUniqueEmail = true;
             }).AddEntityFrameworkStores<ApplicationDbContext>()
@@ -40,18 +47,27 @@ namespace MathTasks {
             .AddDefaultUI();
 
             services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
-            services.AddAWSService<IAmazonS3>();
+            services.AddAWSService<IAmazonS3>(new AWSOptions
+            {
+                Profile = "default",
+                Region = RegionEndpoint.USEast2,
+                Credentials = new BasicAWSCredentials("AKIA6IIOMCEVY65CAXMI", "mMDtm7ljcjGMM8YiE9s6nIV98PQbQ4OPoPexbzZE")
+            });
 
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
-            } else {
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
@@ -64,7 +80,8 @@ namespace MathTasks {
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => {
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
