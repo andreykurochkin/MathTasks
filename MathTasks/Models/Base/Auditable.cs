10e0213af53e852a2645415a384fb73eb2030d3d
andrey kurochkin <andrey.kurochkin@hotmail.com>
2021-11-19 07:29:11 -0800

    added abstract class Auditable,

---
+++ MathTasks/Models/Base/Auditable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathTasks.Models.Base
+{
+    public abstract class Auditable
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdateddBy { get; set; }
+    }
+}
--- MathTasks/Models/MathTask.cs
+++ MathTasks/Models/MathTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathTasks.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
