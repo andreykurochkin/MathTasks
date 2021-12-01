fbb2aa7b4f37f25341053ccdca125b7f5be69263
andrey kurochkin <andrey.kurochkin@hotmail.com>
2021-12-01 18:12:34 +0300

    added tasks summary

--- MathTasks/Startup.cs
+++ MathTasks/Startup.cs
@@ -55,8 +55,11 @@ namespace MathTasks
 
             services.AddMediatR(typeof(Startup));
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
             services.AddRazorPages();
+            services.AddResponseCaching();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
