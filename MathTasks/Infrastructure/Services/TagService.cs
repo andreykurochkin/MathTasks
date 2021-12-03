105420eae43db119b4cf758825a855e21c10aa50
andrey kurochkin <andrey.kurochkin@hotmail.com>
2021-12-03 17:08:22 +0300

    clusterization before refactoring

--- MathTasks/Infrastructure/Services/TagService.cs
+++ MathTasks/Infrastructure/Services/TagService.cs
@@ -30,15 +30,23 @@ namespace MathTasks.Infrastructure.Services
                     Weight = entity.MathTasks == null ? 0 : entity.MathTasks.Count
                 });
             return viewModels;
-
-
-            var foo = new TagCloudViewModelCluster(9, (t) => t.Weight, new List<TagCloudViewModel> { new() });
         }
     }
 
     public class ClusterOptions<T>
     {
+        //private readonly int _startIndex = 0;
+        //private readonly int _endIndex = 0;
 
+        public Func<T, int> OnMember { get; }
+        public int UpperBoundOfClusters { get; }
+
+        public ClusterOptions(Func<T, int> onMember, int upperBoundOfClusters)
+        {
+            //_endIndex = upperBoundOfClusters - 1;
+            OnMember = onMember;
+            UpperBoundOfClusters = upperBoundOfClusters;
+        }
     }
 
     /// <summary>
@@ -46,12 +54,50 @@ namespace MathTasks.Infrastructure.Services
     /// for each instance of TagCloudViewModel creates a number
     /// numbers belong to the range from 0 to count - 1
     /// </summary>
-    public class TagCloudViewModelCluster
+    public class Cluster<T>
     {
-        private readonly int _startIndex = 0;
-        public TagCloudViewModelCluster(int endIndex, Func<TagCloudViewModel, int> onMember, IEnumerable<TagCloudViewModel> items)
-        {
+        private readonly ClusterOptions<T> _options;
+        private readonly IEnumerable<T> _items;
 
+        public Cluster(ClusterOptions<T> options, IEnumerable<T> items)
+        {
+            _options = options;
+            _items = items;
+        }
+
+        public List<Tuple<int, T>> ToList()
+        {
+            var orderedItems = _items.OrderBy(_options.OnMember).ToList();
+
+            var clusters = new List<List<T>>();
+            if (orderedItems.Any())
+            {
+                var min = orderedItems.Min(_options.OnMember);
+                var max = orderedItems.Max(_options.OnMember) + min;
+                var completeRange = max - min;
+                var groupRange = completeRange / (double)_options.UpperBoundOfClusters;
+                var cluster = new List<T>();
+                var currentRange = min + groupRange;
+                for (int i = 0; i < orderedItems.Count; i++)
+                {
+                    while (_options.OnMember(orderedItems.ToArray()[i]) > currentRange)
+                    {
+                        clusters.Add(cluster);
+                        cluster = new List<T>();
+                        currentRange += groupRange;
+                    }
+                    cluster.Add(orderedItems.ToArray()[i]);
+                }
+                clusters.Add(cluster);
+            }
+
+            var result = new List<Tuple<int, T>>();
+
+            clusters.Select((ListOfT, @Index) => new { @Index, ListOfT })
+                .ToList()
+                .ForEach(anonym => result.AddRange(anonym.ListOfT.Select(instanceOfT => new Tuple<int, T>(anonym.Index, instanceOfT))));
+
+            return result;
         }
     }
 }
