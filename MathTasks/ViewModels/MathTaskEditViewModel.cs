5da7fdeb55858542f415138dbd1a420a3061aa9b
andrey kurochkin <andrey.kurochkin@hotmail.com>
2021-12-10 12:35:36 +0300

    added data annotations to view model

--- MathTasks/ViewModels/MathTaskEditViewModel.cs
+++ MathTasks/ViewModels/MathTaskEditViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using MathTasks.Models;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MathTasks.ViewModels
 {
     public class MathTaskEditViewModel
     {
+        [Display(Name = "Theme of Math Task")]
         public string Theme { get; set; } = null!;
 
+        [Display(Name = "Theme of Math Task")]
         public string Content { get; set; } = null!;
 
-        public string ReturnUrl { get; set; }=null!;
+        public string ReturnUrl { get; set; } = null!;
 
-        public IEnumerable<Tag> Tags { get; set; }
+        [Display(Name ="Tags of Math Task")]
+        public IEnumerable<Tag> Tags { get; set; } = null!;
 
+        [Display(Name = "Tags of Math Task")]
+        [Range(1,8, ErrorMessage ="Up to 8 Tags needed")]
         public int TotalTags { get; set; }
     }
 }
