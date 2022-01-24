a6ac91876d2b916c92b7eabe5d5514847d29444c
andrey kurochkin <andrey.kurochkin@hotmail.com>
2022-01-24 15:18:21 +0300

    claims infrastructure created

---
+++ MathTasks/Authorization/Strores.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace MathTasks.Authorization;
+
+public static class Stores
+{
+    public static class ClaimsStore
+    {
+        public static class Names
+        {
+            public const string CreateRole = "Create Role";
+        }
+
+        public static readonly Claim CreateRole = new Claim(Names.CreateRole, false.ToString());
+
+        public static IEnumerable<Claim> GetClaims()
+        {
+            yield return CreateRole;
+        }
+    }
+}
