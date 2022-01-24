825955661c4383363c6d10db5e0405ce8fe9ca41
andrey kurochkin <andrey.kurochkin@hotmail.com>
2022-01-24 15:13:51 +0300

    done

--- MathTasks/Startup.cs
+++ MathTasks/Startup.cs
@@ -20,101 +20,100 @@ using MathTasks.Models;
 using MathTasks.Persistent.Repositories;
 using Kurochkin.Persistence.UnitOfWork;
 
-namespace MathTasks
+namespace MathTasks;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRouting(options =>
         {
-            Configuration = configuration;
-        }
+            options.LowercaseUrls = true;
+            options.LowercaseQueryStrings = true;
+        });
 
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        //    .AddEntityFrameworkStores<ApplicationDbContext>();
+        services.AddIdentity<IdentityUser, IdentityRole>(options =>
         {
-            services.AddRouting(options =>
-            {
-                options.LowercaseUrls = true;
-                options.LowercaseQueryStrings = true;
-            });
+            options.SignIn.RequireConfirmedAccount = true;
+            options.User.RequireUniqueEmail = true;
+        }).AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders()
+        .AddDefaultUI();
 
+        //MapperRegistration.GetMapperConfiguration();
+        services.AddAutoMapper(typeof(Startup).Assembly);
+        services.AddMediatR(typeof(Startup));
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
+        services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddIdentity<IdentityUser, IdentityRole>(options =>
-            {
-                options.SignIn.RequireConfirmedAccount = true;
-                options.User.RequireUniqueEmail = true;
-            }).AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders()
-            .AddDefaultUI();
+        services.AddRazorPages();
 
-            //MapperRegistration.GetMapperConfiguration();
-            services.AddAutoMapper(typeof(Startup).Assembly);
-            services.AddMediatR(typeof(Startup));
+        // dependency injection
+        services.AddTransient<ITagService, TagService>();
+        services.AddTransient<ISearchTagsService, SearchTagsService>();
 
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        services.AddScoped<DbContext, ApplicationDbContext>();
+        services.AddTransient<IRepository<MathTask, Guid>, EFCoreRepository<MathTask, Guid>>();
 
-            services.AddRazorPages();
+        services.AddScoped(typeof(IRepository<MathTask, Guid>), typeof(MathTaskRepository));
 
-            // dependency injection
-            services.AddTransient<ITagService, TagService>();
-            services.AddTransient<ISearchTagsService, SearchTagsService>();
+        services.AddServerSideBlazor();
 
-            services.AddScoped<DbContext, ApplicationDbContext>();
-            services.AddTransient<IRepository<MathTask, Guid>, EFCoreRepository<MathTask, Guid>>();
-            
-            services.AddScoped(typeof(IRepository<MathTask,Guid>), typeof(MathTaskRepository));
+    }
 
-            services.AddServerSideBlazor();
+    private void MathTaskRepository()
+    {
+        throw new NotImplementedException();
+    }
 
-        }
-
-        private void MathTaskRepository()
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            throw new NotImplementedException();
+            app.UseDeveloperExceptionPage();
+            app.UseMigrationsEndPoint();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        else
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "alter",
-                    pattern: "{controller=AlterMathTasks}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-                endpoints.MapBlazorHub();
-            });
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "alter",
+                pattern: "{controller=AlterMathTasks}/{action=Index}/{id?}");
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+            endpoints.MapBlazorHub();
+        });
     }
 }
